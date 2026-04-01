7f4d4757a6a6a90e7d2317109a6f9320c1627413
Mikey <mvpunla@gmail.com>
2026-04-01 14:14:55 -0700

    Setup arcjet sercurity

--- proxy.ts
+++ proxy.ts
@@ -1,8 +1,31 @@
+import arcjet, { detectBot, shield, slidingWindow } from "@arcjet/next";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)"]);
 
+const aj = arcjet({
+  key: process.env.ARCJET_KEY!,
+  rules: [
+    detectBot({
+      allow: ["CATEGORY:MONITOR", "CATEGORY:PREVIEW", "CATEGORY:SEARCH_ENGINE"],
+      mode: "LIVE",
+    }),
+    shield({ mode: "LIVE" }),
+    slidingWindow({
+      interval: "1m",
+      max: 100,
+      mode: "LIVE",
+    }),
+  ],
+});
+
 export default clerkMiddleware(async (auth, req) => {
+  const decision = await aj.protect(req);
+
+  if (decision.isDenied()) {
+    return new Response("Access denied", { status: 403 });
+  }
+
   if (!isPublicRoute(req)) {
     await auth.protect();
   }
